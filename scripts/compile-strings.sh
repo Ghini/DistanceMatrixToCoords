299ef644ab1a6c4d124265456c288ddca779ebc2
Mario Frasca <mario@anche.no>
2017-01-06 15:15:14 -0500

    do not compile translation if nothing changed

--- scripts/compile-strings.sh
+++ scripts/compile-strings.sh
@@ -5,8 +5,12 @@ LOCALES=$2
 
 for LOCALE in ${LOCALES}
 do
-    echo "Processing: ${LOCALE}.ts"
-    # Note we don't use pylupdate with qt .pro file approach as it is flakey
-    # about what is made available.
-    $LRELEASE i18n/${LOCALE}.ts
+    if [[ i18n/${LOCALE}.ts -nt i18n/latest_run ]]
+    then
+        echo "Processing: ${LOCALE}.ts"
+        # Note we don't use pylupdate with qt .pro file approach as it is flakey
+        # about what is made available.
+        $LRELEASE i18n/${LOCALE}.ts
+    fi
 done
+touch i18n/latest_run
